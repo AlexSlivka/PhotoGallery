b647ffb4989c10914127c58266b24b7974bacb7b
Aleksei Slivka <alexprospero25@gmail.com>
2021-06-29 22:28:34 +0300

    Добавил меню с SearchView для поиска фото на сайте по введеному слову.

--- app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.photogallery
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.example.photogallery.api.FlickrFetchr
 
@@ -8,7 +10,17 @@ class PhotoGalleryViewModel : ViewModel() {
 
     val galleryItemLiveData: LiveData<List<GalleryItem>>
 
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
+
     init {
-        galleryItemLiveData = FlickrFetchr().searchPhotos("bicycle")
+        mutableSearchTerm.value = "planets"
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
+            flickrFetchr.searchPhotos(searchTerm)
+        }
+    }
+
+    fun fetchPhotos(query: String = "") {
+        mutableSearchTerm.value = query
     }
 }
