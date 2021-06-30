df1f47d4e4cb2bc5560609f1b8bbedaa43539c09
Aleksei Slivka <alexprospero25@gmail.com>
2021-06-30 21:47:11 +0300

    Сохранение последнего поискового запроса с помошью SharedPreferences.

--- app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -1,26 +1,31 @@
 package com.example.photogallery
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
 import com.example.photogallery.api.FlickrFetchr
 
-class PhotoGalleryViewModel : ViewModel() {
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
 
     val galleryItemLiveData: LiveData<List<GalleryItem>>
 
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
+    val searchTerm: String
+        get() = mutableSearchTerm.value ?: ""
 
     init {
         mutableSearchTerm.value = "planets"
         galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
-            flickrFetchr.searchPhotos(searchTerm)
+            if (searchTerm.isBlank()) {
+                flickrFetchr.fetchPhotos()
+            } else {
+                flickrFetchr.searchPhotos(searchTerm)
+            }
         }
     }
 
     fun fetchPhotos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
         mutableSearchTerm.value = query
     }
 }
